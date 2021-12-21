e91172746fa1b685a535fcedc1bfd51890d3a587
Daniel Prates <danprates94@gmail.com>
2021-12-21 11:39:05 -0300

    feat: add git config

--- src/post-install/git.sh
+++ src/post-install/git.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
+# config user
 git config --global user.name "Daniel Prates"
 git config --global user.email "danprates94@gmail.com"
 
+# config alias
+git config --global alias.s '!git status -s'
+git config --global alias.c '!git add --all && git commit -m'
+git config --global alias.l '!git log --graph --pretty=format:"%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset" --abbrev-commit'
+git config --global alias.f '!git fetch --all --prune'
+git config --global alias.b '!git branch -av'
+git config --global alias.amend = '!git add --all && git commit --amend --no-edit'
+
+# Other configs
+git config --global pull.rebase true
+git config --global push.followTags true
+git config --global init.defaultBranch master
+
+# install git flow
 sudo apt install git-flow
