d6c517a5d5045388aa5d282d08327e7c2d108d45
Daniel Prates <danprates94@gmail.com>
2022-04-12 09:49:52 -0300

    feat: add global gitignore file

--- src/post-install/git.sh
+++ src/post-install/git.sh
@@ -16,6 +16,7 @@ git config --global alias.amend = '!git add --all && git commit --amend --no-edi
 git config --global pull.rebase true
 git config --global push.followTags true
 git config --global init.defaultBranch master
+git config --global core.excludesfile ~/.config/.gitignore
 
 # install git flow
 sudo apt install git-flow
