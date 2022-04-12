9a030dd881abfc419ae5bfcd6328d72a6e7c768c
Daniel Prates <danprates94@gmail.com>
2022-04-12 10:04:10 -0300

    fix: alias in git config

--- src/post-install/git.sh
+++ src/post-install/git.sh
@@ -5,12 +5,12 @@ git config --global user.name "Daniel Prates"
 git config --global user.email "danprates94@gmail.com"
 
 # config alias
-git config --global alias.s '!git status -s'
-git config --global alias.c '!git add --all && git commit -m'
-git config --global alias.l '!git log --graph --pretty=format:"%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset" --abbrev-commit'
-git config --global alias.f '!git fetch --all --prune'
-git config --global alias.b '!git branch -av'
-git config --global alias.amend = '!git add --all && git commit --amend --no-edit'
+git config --global --replace-all alias.s '!git status -s'
+git config --global --replace-all alias.c '!git add --all && git commit -m'
+git config --global --replace-all alias.l '!git log --graph --pretty=format:"%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset" --abbrev-commit'
+git config --global --replace-all alias.f '!git fetch --all --prune'
+git config --global --replace-all alias.b '!git branch -av'
+git config --global --replace-all alias.amend '!git add --all && git commit --amend --no-edit'
 
 # Other configs
 git config --global pull.rebase true
