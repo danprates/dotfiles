b3e7fd5f55861656800c95649e4aef874d0c897c
Daniel Prates <danprates94@gmail.com>
2024-04-27 19:36:33 -0300

    feat: add PHP & Laravel to vscode installation

--- src/post-install/vscode.sh
+++ src/post-install/vscode.sh
@@ -5,32 +5,58 @@ sudo apt install code
 
 # Install extensions
 
-# Dracula theme
-code --install-extension dracula-theme.theme-dracula
+## Utils
+### Wakatime
+code --install-extension wakatime.vscode-wakatime
+### Spell checker EN
+code --install-extension streetsidesoftware.code-spell-checker
+### Editor config
+code --install-extension EditorConfig.EditorConfig
+### Gitblame
+code --install-extension waderyan.gitblame
+### PlantUML
+code --install-extension jebbs.plantuml
+### File Utils
+code --install-extension sleistner.vscode-fileutils
+### Tailwind
+code --install-extension bradlc.vscode-tailwindcss
+code --install-extension stivo.tailwind-fold
 
-# Material icon theme
+## Visual
+### Dracula theme
+code --install-extension dracula-theme.theme-dracula
+### Material icon theme
 code --install-extension PKief.material-icon-theme
 
-# Editor config
-code --install-extension EditorConfig.EditorConfig
-
-# Eslint
+## Typescript & Javascript
+### Eslint
 code --install-extension dbaeumer.vscode-eslint
-
-# Gitblame
-code --install-extension waderyan.gitblame
-
-# Wakatime
-code --install-extension wakatime.vscode-wakatime
-
-# Spell checker
-code --install-extension streetsidesoftware.code-spell-checker
-
-# Liveshare
-code --install-extension ms-vsliveshare.vsliveshare
-
-# PlantUML
-code --install-extension jebbs.plantuml
-
-# Prettier
+### Prettier
 code --install-extension esbenp.prettier-vscode
+
+## Csharp & .NET
+### Intellisense & Dev kit
+code --install-extension ms-dotnettools.vscode-dotnet-runtime
+code --install-extension ms-dotnettools.csharp
+code --install-extension ms-dotnettools.csdevkit
+code --install-extension ms-dotnettools.vscodeintellicode-csharp
+
+## PHP & Laravel
+### Intelephense
+code --install-extension bmewburn.vscode-intelephense-client
+### Namespace Resolver
+code --install-extension MehediDracula.php-namespace-resolver
+### Laravel Blade
+code --install-extension cjhowe7.laravel-blade
+### Laravel Goto
+code --install-extension codingyu.laravel-goto-view
+### Laravel Extra Intellisense
+code --install-extension amiralizadeh9480.laravel-extra-intellisense
+### Blade Formatter
+code --install-extension shufo.vscode-blade-formatter
+### Laravel Artisan
+code --install-extension ryannaddy.laravel-artisan
+
+## Golang
+### Intellisense
+code --install-extension golang.go
